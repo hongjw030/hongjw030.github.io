670cfc8c98e03458a906e8990bfb3823ebb7cf64
HellenHong <hongjw030@naver.com>
2024-03-01 23:33:35 +0900

    feat: 프로필 생성

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,3 +1,24 @@
+import ProfileImg from "/public/profile.jpg";
+
+import Styles from "./Profile.module.scss";
+import Image from "next/image";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className={Styles["profile-container"]}>
+      <Image
+        src={ProfileImg}
+        alt="profile img"
+        width={92}
+        height={92}
+        priority
+        className={Styles["profile-img"]}
+      />
+      <h2 className={Styles["title"]}>홍장군의 개발 일지</h2>
+      <div className={Styles["info"]}>
+        <span className={Styles["field"]}>Frontend Developer</span>
+        <span className={Styles["author"]}>Hongjw</span>
+      </div>
+    </div>
+  );
 }
