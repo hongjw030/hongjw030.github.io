ea6b0e7808a8d28dd2c72e612c7fc18bbc995992
HellenHong <hongjw030@naver.com>
2024-03-03 22:04:47 +0900

    delete

--- src/pages/category.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/layouts/MainLayout";
-
-export default function CategoryPage() {
-  return (
-    <MainLayout selected="category">
-      <div>카테고리 페이지!</div>
-    </MainLayout>
-  );
-}
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,5 +1,5 @@
 import MainLayout from "@/layouts/MainLayout";
 
-export default function HomePage() {
+export default function TestPage() {
   return <MainLayout />;
 }
