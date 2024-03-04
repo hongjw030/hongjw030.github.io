ecd7d49a0c8d12df790b6f2864315122ce46834b
HellenHong <hongjw030@naver.com>
2024-03-05 00:20:27 +0900

    a

---
+++ src/pages/[post]/index.tsx
@@ -0,0 +1,10 @@
+import BlogLayout from "@/layouts/BlogLayout";
+import React from "react";
+
+export default function BlogPage() {
+  return (
+    <BlogLayout>
+      <div>각 포스트..</div>
+    </BlogLayout>
+  );
+}
