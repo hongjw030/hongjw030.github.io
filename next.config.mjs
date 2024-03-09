6c0e38a42ba895875a51472d49446cb210e11d3c
HellenHong <hongjw030@naver.com>
2024-03-10 01:27:17 +0900

    feat:seo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  siteUrl: 'http://localhost:3000' || 'https://hongjw030-github-io.vercel.app/',
   reactStrictMode: true,
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
 };
 
 export default nextConfig;
--- src/constants/user.ts
+++ src/constants/user.ts
@@ -3,6 +3,6 @@ const JOB = "Frontend Developer";
 const NICKNAME = "Hongjw";
 const EMAIL = "hongjw030@naver.com";
 const GITHUB_URL = "https://github.com/hongjw030";
-const BLOG_URL = "https://hongjw030.github.io/";
+const BLOG_URL = "https://hongjw030-github-io.vercel.app";
 
 export {BLOG_TITLE, JOB, NICKNAME, EMAIL, GITHUB_URL, BLOG_URL};
