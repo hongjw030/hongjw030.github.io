9b29212976d6c2ffd83c51abf0aefb4a0a0d211c
HellenHong <hongjw030@naver.com>
2024-03-09 23:59:05 +0900

    FIX

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 
 import "@/styles/globals.scss";
 import "@/styles/mixin.scss";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
-import { Suspense } from "react";
-import InitialComponent from "@/components/initialComponent/InitialComponent";
-
-const queryClient = new QueryClient();
 
 const theme = createTheme({
   typography: {
@@ -18,15 +13,10 @@ const theme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider>
-        <Suspense fallback="Loading...">
-          <ThemeProvider theme={theme}>
-            <InitialComponent />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </Suspense>
-      </Provider>
-    </QueryClientProvider>
+    <Provider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
