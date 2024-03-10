d6323c6818781313bb3e0c5e2f5e5155705d4d1f
HellenHong <hongjw030@naver.com>
2024-03-11 03:14:13 +0900

    feat:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
 import MainLayout from "@/layouts/MainLayout";
-import BlogLayout from "@/layouts/BlogLayout";
 import React from "react";
 import fs from "fs";
 import matter from "gray-matter";
-import { POST_DIR } from "@/constants";
+import { POST_DIR, PROJECT_DIR } from "@/constants";
 import { PostFrontMatterType } from "@/types/PostType";
 import { CarouselSection } from "@/components/homeComponents/CarouselSection";
 import { Box } from "@mui/material";
+import { ProjectType } from "@/types/ProjectType";
 
-export default function HomePage({ sorted }: any) {
-  // 맨 처음엔 자기 소개 header 더보기 누르면 about 페이지로 이동.
-
+export default function HomePage({ sortedPosts, sortedProjects }: any) {
   return (
     <MainLayout>
       <Box
@@ -21,11 +19,15 @@ export default function HomePage({ sorted }: any) {
           alignItems: "center",
         }}
       >
-        <CarouselSection label="최신 포스팅" link="blog" cardList={sorted} />
+        <CarouselSection
+          label="최신 포스팅"
+          link="blog"
+          cardList={sortedPosts}
+        />
         <CarouselSection
           label="최신 프로젝트"
           link="project"
-          cardList={sorted}
+          cardList={sortedProjects}
         />
       </Box>
     </MainLayout>
@@ -33,20 +35,19 @@ export default function HomePage({ sorted }: any) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(POST_DIR);
+  const postFiles = fs.readdirSync(POST_DIR);
+  const projectFiles = fs.readdirSync(PROJECT_DIR);
 
-  const posts = files.map((filename) => {
+  const posts = postFiles.map((filename) => {
     const markdownWithMetadata = fs
       .readFileSync(`src/_posts/${filename}`)
       .toString();
     let stats = fs.statSync(`src/_posts/${filename}`);
 
     const { data } = matter(markdownWithMetadata);
-
     const frontmatter = {
       ...data,
     } as PostFrontMatterType;
-
     return {
       slug: filename.replace(".md", ""),
       frontmatter,
@@ -55,19 +56,36 @@ export async function getStaticProps() {
     };
   });
 
-  let sortedPosts = posts.filter((el) => el !== null);
+  const projects = projectFiles.map((filename) => {
+    const markdownWithMetadata = fs
+      .readFileSync(`src/_projects/${filename}`)
+      .toString();
 
-  if ((sortedPosts.length == 1 && sortedPosts[0] == null) || !posts) {
+    const { data } = matter(markdownWithMetadata);
+    const frontmatter = {
+      ...data,
+    } as ProjectType;
     return {
-      props: { sortedPosts: [] },
+      slug: filename.replace(".md", ""),
+      frontmatter,
     };
-  } else {
-    sortedPosts = posts.sort((a, b) =>
-      (a?.birthTime ?? 1) < (b?.birthTime ?? 1) ? 1 : -1
-    );
-    let sorted = sortedPosts.slice(0, 5);
-    return {
-      props: { sorted },
-    };
-  }
+  });
+
+  let sortedPosts = posts.filter((el) => el !== null);
+  let sortedProjects = projects.filter((el) => el !== null);
+
+  sortedPosts = sortedPosts.sort((a, b) =>
+    (a?.birthTime ?? 1) < (b?.birthTime ?? 1) ? 1 : -1
+  );
+
+  sortedProjects = sortedProjects.sort((a, b) =>
+    a?.frontmatter.note < b?.frontmatter.note ? 1 : -1
+  );
+
+  return {
+    props: {
+      sortedPosts: sortedPosts.slice(0, 5),
+      sortedProjects: sortedProjects,
+    },
+  };
 }
