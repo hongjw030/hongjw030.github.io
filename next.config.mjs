7b8117c444fe7b0c32f56428cb5d2ab41b67023c
HellenHong <hongjw030@naver.com>
2024-03-10 14:48:05 +0900

    chore

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  siteUrl: 'http://localhost:3000' || 'https://hongjw030-github-io.vercel.app/',
   reactStrictMode: true,
-  generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'],
 };
 
 export default nextConfig;
