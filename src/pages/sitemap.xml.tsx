1076e139b044a05406871c5eb2d97dcaccd8430d
HellenHong <hongjw030@naver.com>
2024-03-13 00:34:12 +0900

    refactor

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -52,6 +52,12 @@ export async function getServerSideProps({ res }: any) {
 
   let sitemapString = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://hongjw030-github-io.vercel.app</loc>
+<changefreq>daily</changefreq>
+<priority>0.8</priority>
+</url>
+
 ${totalUrl}
 </urlset>`;
 
