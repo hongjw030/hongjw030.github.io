8c11c98224c0d108cfd0d3b0d79bf2272427f5ac
HellenHong <hongjw030@naver.com>
2024-03-13 00:08:56 +0900

    refactor: robots

---
+++ src/pages/robots.txt.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function RobotsPage() {
+  return (
+    <>
+      User-agent: *
+      <br />
+      Allow: / Sitemap:
+      <br />
+      https://hongjw030-github-io.vercel.app/sitemap.xml
+    </>
+  );
+}
