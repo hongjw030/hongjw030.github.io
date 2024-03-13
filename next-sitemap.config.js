18d12226fd823d1721ce65c6881b0c48ae2fcd53
HellenHong <hongjw030@naver.com>
2024-03-13 18:52:01 +0900

    refactor

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,10 +3,17 @@ const siteUrl = "https://hongjw030-github-io.vercel.app";
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 1, 
   robotsTxtOptions: {
     policies: [{ userAgent: "*", allow: "/" }],
     additionalSitemaps: [
       `${siteUrl}/sitemap.xml`,
+      `${siteUrl}/sitemap.xml/sitemap-projectid.xml`,
+      `${siteUrl}/sitemap.xml/sitemap-post.xml`,
+      `${siteUrl}/sitemap.xml/sitemap-mainid.xml`,
+      `${siteUrl}/sitemap.xml/sitemap-subid.xml`,
     ],
   },
 };
