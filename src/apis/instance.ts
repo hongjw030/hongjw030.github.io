f18805ead7d9f0df64850094a48757b94166abbc
HellenHong <hongjw030@naver.com>
2024-03-20 21:30:58 +0900

    test

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: `https://hongjw030-github-io.vercel.app/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_LINK}/api`,
 })
 
 export default axiosInstance
