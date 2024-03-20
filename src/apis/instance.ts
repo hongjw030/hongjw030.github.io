672b9bf04a077c2e70819d7c3d0d0a6608943794
HellenHong <hongjw030@naver.com>
2024-03-20 21:21:33 +0900

    post

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `https://hongjw030-github-io.vercel.app/api`,
 })
 
 export default axiosInstance
