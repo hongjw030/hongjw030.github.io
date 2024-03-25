efb41639bb2a8b689b32645ea975bf75ae759482
HellenHong <hongjw030@naver.com>
2024-03-25 23:45:24 +0900

    feat: edit 페이지 생성

--- src/pages/api/project/[projectId].ts
+++ src/pages/api/project/[projectId].ts
@@ -7,6 +7,19 @@ export default async function handler(req: any, res: any){
   await dbConnect();
 
   const {projectId} = req.query;
-  const myPost = await project.findById(projectId);
-  res.status(201).send(myPost);
+
+  switch(req.method){
+    case 'PATCH': 
+      const updatedMyPost = await project.findByIdAndUpdate(projectId, req.body);
+      res.status(201).send({message: "update success!", data: updatedMyPost })
+      break;
+    case 'GET': 
+      const myPost = await project.findById(projectId);
+      res.status(201).send(myPost);
+      break;
+    case 'DELETE':
+      await project.findByIdAndDelete(projectId);
+      res.status(204).send({message: "deleted Success!!"});
+      break;
+  }
 }
